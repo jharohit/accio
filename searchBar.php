d0ffa11ce2b6b2ba3a1326b08834b2a790520620
Rohit Jha <rj@rohitjha.com>
2013-03-08 02:08:56 +0800

    Initial Commit of Files - Facebook search via graph api

---
+++ searchBar.php
@@ -0,0 +1,6 @@
+<form class="form-search" method="post" action="search.php">
+
+    <input name="search" type="text" placeholder="Dhoond lo!" class="input-xxxlarge search-query">
+    <button class="btn btn-primary" id="submit" type="submit">search </button>
+
+</form>
