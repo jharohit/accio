48a4bc60d2a91b91e3cf4d8edbfed04d3409f89b
Rohit Jha <rj@rohitjha.com>
2013-03-08 02:09:52 +0800

    Revert "Initial Commit of Files - Facebook search via graph api"
    This reverts commit d0ffa11ce2b6b2ba3a1326b08834b2a790520620.

--- searchBar.php
+++
@@ -1,6 +0,0 @@
-<form class="form-search" method="post" action="search.php">
-
-    <input name="search" type="text" placeholder="Dhoond lo!" class="input-xxxlarge search-query">
-    <button class="btn btn-primary" id="submit" type="submit">search </button>
-
-</form>
