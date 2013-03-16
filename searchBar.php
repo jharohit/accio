68d893685882915f6934ec0d23d2dbe9104820d7
Rohit Jha <rj@rohitjha.com>
2013-03-16 21:54:23 +0800

    search bar changes

--- searchBar.php
+++ searchBar.php
@@ -1,6 +1,6 @@
-<form method="get" action="./<?php echo $page; ?>">
+<form class="form-horizontal" method="get" action="./<?php echo $page; ?>">
 
-    <input type="text" placeholder="Dhoond lo!" class="input-xxxlarge search-query">
+    <input name="search" type="text" placeholder="Dhoond lo!" class="input-xxxlarge">
     <button class="btn btn-primary" id="submit" type="submit">search </button>
 
 </form>
