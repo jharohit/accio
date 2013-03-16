c2fedcfb51f0a610a93deb6b4d09b783970f5213
Rohit Jha <rj@rohitjha.com>
2013-03-16 21:29:10 +0800

    updated wrong search and input button type to become responsive

--- searchBar.php
+++ searchBar.php
@@ -1,6 +1,6 @@
-<form class="form-search" method="get" action="<?php echo $page; ?>">
+<form method="get" action="./<?php echo $page; ?>">
 
-    <input name="search" type="text" placeholder="Dhoond lo!" class="input-xxxlarge search-query">
+    <input type="text" placeholder="Dhoond lo!" class="input-xxxlarge search-query">
     <button class="btn btn-primary" id="submit" type="submit">search </button>
 
 </form>
