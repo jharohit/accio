da551f95c0421321c0013a3c1542af6cff13c08f
Rohit Jha <rj@rohitjha.com>
2013-04-05 23:31:51 +0800

    resolved the search error

--- searchBar.php
+++ searchBar.php
@@ -1,5 +1,5 @@
-<form class="form-horizontal" method="get" action="./<?php echo $page; ?>">
-
+<form class="form-horizontal" method="get" action="/<?php echo $page; ?>">
+    <?php echo $page; ?>
     <input name="search" type="text" placeholder="Dhoond lo!" class="input-xxxlarge">
     <button class="btn btn-primary" id="submit" type="submit">search </button>
 
