b7c5902dcfd5e3aca7310e87035fac41e84371ba
Rohit Jha <rj@rohitjha.com>
2013-04-05 23:59:19 +0800

    updated searchpage

--- searchBar.php
+++ searchBar.php
@@ -1,5 +1,4 @@
 <form class="form-horizontal" method="get" action="/<?php echo $page; ?>">
-    <?php echo $page; ?>
     <input name="search" type="text" placeholder="Dhoond lo!" class="input-xxxlarge">
     <button class="btn btn-primary" id="submit" type="submit">search </button>
 
